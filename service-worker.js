73f73318b04b9505b84a4c09ce681b72c760a270
Your Name <graceliu@gracelius-iMac.home>
2022-02-09 14:02:54 -0500

    added manifest

--- service-worker.js
+++ service-worker.js
@@ -14,31 +14,6 @@ const FILES_TO_CACHE = [
     "./dist/tickets.bundle.js",
     "./dist/schedule.bundle.js"
   ];
-
-self.addEventListener('install',function(e){
-    e.waitUntil(
-        caches.open(CACHE_NAME).then(function(cache){
-            console.log("installing cache: " + CACHE_NAME);
-            return cache.addAll(FILES_TO_CACHE)
-        })
-    )
-})
-self.addEventListener('activate', function(e){
-    e.waitUntil(
-        caches.keys().then(function(keyList){
-            let cacheKeeplist = keyList.filter(function(key){
-                return key.indexOf(APP_PREFIX)
-            })
-            cacheKeeplist.push(CACHE_NAME);
-            return Promise.all(keyList.map(function(key,i){
-                if(cacheKeeplist.indexOf(key)=== -1){
-                    console.log('deleting cache:' + keyList[i]);
-                    return caches.delete(keyList[i])
-                }
-            }))
-        })
-    )
-})
 self.addEventListener('fetch', function(e){
     console.log('fetch request: ' + e.request.url)
     e.respondWith(
@@ -52,6 +27,32 @@ self.addEventListener('fetch', function(e){
             }
             // you can omit if/else for console.log& put one line below like this too
             // return request || fetch(e.request)
+        })
+    )
+})
+
+self.addEventListener('install',function(e){
+    e.waitUntil(
+        caches.open(CACHE_NAME).then(function(cache){
+            console.log("installing cache: " + CACHE_NAME);
+            return cache.addAll(FILES_TO_CACHE)
+        })
+    )
+})
+
+self.addEventListener('activate', function(e){
+    e.waitUntil(
+        caches.keys().then(function(keyList){
+            let cacheKeeplist = keyList.filter(function(key){
+                return key.indexOf(APP_PREFIX)
+            })
+            cacheKeeplist.push(CACHE_NAME);
+            return Promise.all(keyList.map(function(key,i){
+                if(cacheKeeplist.indexOf(key)=== -1){
+                    console.log('deleting cache:' + keyList[i]);
+                    return caches.delete(keyList[i])
+                }
+            }))
         })
     )
 })
